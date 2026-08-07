942380d01bc33ff412543c2731baa755fcfed1cc
DVitaliy <dynnik.vitaliy@gmail.com>
2026-08-07 15:47:03 +0300

    init

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import MainLayout from '@/layouts/MainLayout.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: MainLayout,
+      children: [
+        { path: '', component: () => import('@/pages/HomeView.vue') },
+      ],
+    },
+  ],
+})
+
+export default router
