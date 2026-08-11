85161ae67cfbf15f042121cdcf0c041a8c16fcd5
vadonchikus <vadonchikus@users.noreply.github.com>
2026-08-11 10:22:36 +0300

    Update and enlarge Epicentr logo

--- src/components/ui/PlatformLogo.vue
+++ src/components/ui/PlatformLogo.vue
@@ -13,7 +13,10 @@ const logoSources: Record<string, string> = {
     v-if="logoSources[props.platform]"
     :src="logoSources[props.platform]"
     :alt="props.platform"
-    class="inline-block h-[1.65em] w-auto max-w-32 object-contain align-[-0.14em]"
+    :class="[
+      'inline-block w-auto object-contain align-[-0.14em]',
+      props.platform === 'Эпицентр' ? 'h-[2em] max-w-36' : 'h-[1.65em] max-w-32',
+    ]"
   />
   <span v-else>{{ props.platform }}</span>
 </template>
